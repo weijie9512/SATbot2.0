e87a5b399e3d5428c787efcffa2111e23a45c4fd
Wei Jie Chua <weijiechua9512@gmail.com>
2022-06-05 20:38:50 +0100

    Delete newsfeed.py

--- auc/newsfeed.py
+++
@@ -1,7 +0,0 @@
-from newsapi import NewsApiClient
-from pandas.io.json import json_normalize
-import pandas as pd
-
-
-api_key = "188b1d9e2e794b0e98cb08db39f8d2e5"
-newsapi = NewsApiClient(api_key=api_key)
